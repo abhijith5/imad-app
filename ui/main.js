f05f35a075e0c86d4c2f4d4b9d44d3082fcd9692
abhijith5 <abhijithchavarkkad@gmail.com>
2018-02-20 22:53:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
+    console.warn("Sda")
     //make a requset to the counter endpoint
     
     //Capture the response and store to a variable
